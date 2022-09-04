e98ab72356fef33696f768c0750604ec50a62f88
Truscot <truscottchimenem@gmail.com git config --global user.name>
2022-09-04 13:43:08 +0100

    initial commit

---
+++ celo-nft-marketplace/frontend/constants.js
@@ -0,0 +1,3 @@
+export const SUBGRAPH_URL =
+  "https://api.thegraph.com/subgraphs/name/danielamadi01/celo-nft-marketplace";
+export const MARKETPLACE_ADDRESS = "0xd87a612835eebE79747D1aED689CFc4EdD6a629A";
